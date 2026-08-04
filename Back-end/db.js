d23bcc7cdf30368ec71bd0208177a9777b9e28e4
ViktoZZ <122486111+ViktoZZ@users.noreply.github.com>
2026-08-04 11:47:25 +1000

    DB connection process

---
+++ Back-end/db.js
@@ -0,0 +1,18 @@
+const mysql = require('mysql2');
+
+const db = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'bitebite',
+  database: 'db_bite_blog'
+});
+
+db.connect((err) => {
+  if (err) {
+    console.error('Erro ao conectar ao MySQL:', err);
+    return;
+  }
+  console.log('Conectado ao MySQL');
+});
+
+module.exports = db;
